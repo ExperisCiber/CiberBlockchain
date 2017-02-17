a69d6cce0b4491f80491e6e05e0d6c7b790d5c4d
Stijn Koopal <stijn.koopal@gmail.com>
2017-02-17 14:06:33 +0000

    works on cloud9 now

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,9 +3,9 @@ const webpack = require('webpack');
 
 module.exports = {
   context: path.resolve(__dirname, './src'),
-  entry: {
-    app: './app.js'
-  },
+  entry: [
+    './app.js'
+  ],
   output: {
     path: path.resolve(__dirname, './dist/assets'),
     filename: '[name].bundle.js',
