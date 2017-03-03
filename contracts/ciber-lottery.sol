02ce6c42a38fe29305a9adcb5c60265eb6cc82b0
Iemke Kooijman <iemkek@live.nl>
2017-03-03 09:24:12 +0000

    message

--- contracts/ciber-lottery.sol
+++ contracts/ciber-lottery.sol
@@ -4,12 +4,11 @@
 
    contract ciberLottery {
 
-      event BuyIn();
-      event LotteryStart();
-      event LotteryEnd();
-      
-       address owner;
+       event BuyIn();
+       event LotteryStart();
+       event LotteryEnd();
 
+       address owner;
        bool public lotteryState;
 
        struct Ticket {
@@ -17,28 +16,23 @@
            uint ticketNumber;
        }
        mapping(uint => Ticket) tickets;
-       
-       
-
        uint ticketCounter;
-
        bytes32 lotteryTitle;
        uint endDateStart; // expected format: unix timestamp
        uint endDateClose; // expected format: unix timestamp
        uint ticketPrice; // expected format: whole numbers in ether
        uint ticketMax; // expected format: whole numbers
-
+       bool testFlag;
        bytes32 check;
-       uint public random;
-       
+       uint256 public random;
        address winner;
        uint price;
-       
+
        function ciberLottery() {
            owner = msg.sender;
        }
 
-       function startLottery(bytes32 _lotteryTitle, uint _endDateStart, uint _endDateClose, uint _ticketPrice, uint _ticketMax) {
+       function startLottery(bytes32 _lotteryTitle, uint _endDateStart, uint _endDateClose, uint _ticketPrice, uint _ticketMax, bool _testFlag) {
 
            if (owner == msg.sender) {
                if (!lotteryState) {
@@ -48,7 +42,8 @@
                    endDateClose = _endDateClose;
                    ticketPrice = _ticketPrice;
                    ticketMax = _ticketMax;
-                   
+                   testFlag = _testFlag;
+
                    LotteryStart();
                }
                else {
@@ -66,16 +61,20 @@
            // Alleen door lottery owner
            if (msg.sender == owner) {
 
+               // Alleen als lottery is gestart
                if (lotteryState) {
 
                    // Alleen OP lottery endDate
-                   //if (now > endDateStart && now < endDateClose) {
-                       random = 3;
+                   if ((now > endDateStart && now < endDateClose) || testFlag) {
+                       random = ((now * now * now) % 10 ** 1);
+                       while (random > ticketCounter) {
+                           random = ((now * now * now) % 10 ** 1);
+                       }
                        winner = tickets[random].participant;
                        price = this.balance;
                        tickets[random].participant.send(this.balance);
 
-                        // Stop lottery
+                       // Stop lottery
                        lotteryState = false;
                        lotteryTitle = '';
                        endDateStart = 0;
@@ -83,33 +82,37 @@
                        ticketPrice = 0;
                        ticketMax = 0;
                        ticketCounter = 0;
-                       
+
                        LotteryEnd();
 
-                   //} else throw;
-               } else throw;
-           } else throw;
+                   }
+                   else throw;
+               }
+               else throw;
+           }
+           else throw;
        }
 
 
-       function buyIn() payable returns (uint) {
+       function buyIn() payable returns(uint) {
 
-           if (now < endDateStart) {
-               
+           if (now < endDateStart || testFlag) {
+
                if (msg.value == ticketPrice) {
 
                    ticketCounter++;
                    if (ticketCounter <= ticketMax) {
-                   
-                   tickets[ticketCounter] = Ticket({
-                       participant: msg.sender,
-                       ticketNumber: ticketCounter
-                   });
-                   
-                   BuyIn();
-                   return (ticketCounter);
-                   
-                   } else {
+
+                       tickets[ticketCounter] = Ticket({
+                           participant: msg.sender,
+                           ticketNumber: ticketCounter
+                       });
+
+                       BuyIn();
+                       return (ticketCounter);
+
+                   }
+                   else {
                        ticketCounter = ticketCounter - 1;
                        throw;
                    }
@@ -128,15 +131,18 @@
        function refund() {
 
            // Alleen NA endDate
-           //if (now > endDateClose) {
-           
-             for(uint i = 0; i <= ticketCounter; i++) {
-               if (tickets[i].participant == msg.sender) {
-                tickets[i].participant.send(ticketPrice);
-                delete tickets[i];
+           if (now > endDateClose || testFlag) {
+
+               if (lotteryState) {
+
+                   for (uint i = 0; i <= ticketCounter; i++) {
+                       if (tickets[i].participant == msg.sender) {
+                           tickets[i].participant.send(ticketPrice);
+                           delete tickets[i];
+                       }
+                   }
                }
-             }
-           //}
+           }
        }
 
 
